c571d4a4548de8291774294aa45a8fb164e19932
Rupam Kanjilal <rupam386595@gmail.com>
2024-05-19 02:52:14 +0530

    A blog website using react and appwrite 2

--- MegaBlogWebsite/src/components/Logo.jsx
+++ MegaBlogWebsite/src/components/Logo.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
-function Logo({width = '100px'}) {
-  return (
-    <div>Logo</div>
-  )
+function Logo({ width = "100px" }) {
+  return <div className="text-2xl font-bold">RUPAM BLOG</div>;
 }
 
-export default Logo
+export default Logo;
